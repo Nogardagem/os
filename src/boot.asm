dca312119990ab5a83edce13e160c52a84ab361d
Nogardagem <understateddragon@gmail.com>
2025-09-23 17:21:52 -0400

    scrolling text

--- src/boot.asm
+++ src/boot.asm
@@ -1,48 +1,94 @@
-org 7C00h ;this line is necessary for jump offsets that end up being absolute
+org 7C00h
+
+jmp short Start ;Jump over the data (the 'short' keyword makes the jmp instruction smaller)
+
+vmem equ 1000h
+
+printal:
+    pushad
+    mov ah,2 ;cursor pos
+    int 10h
+    
+    mov ah, 9 ;print
+    int 10h
+
+    add dx,vmem
+    mov bl,0
+    push 0
+    pop es
+    mov di,dx
+    mov [es:di],al
+    
+    popad
+    inc dl
+ret
+printnl:
+    pushad
+
+    push 0
+    pop es
+    mov dx,0
+    mov dh,0
+reprintloop:
+    mov di,vmem
+    inc dh
+    add di,dx
+    mov al,[es:di]
+    dec dh
+    ;mov si,0
+    ;mov [es:di],si
+    
+    call printal
+    
+    
+    cmp dl,80
+    jne skipreprintrow
+    inc dh
+    mov dl,0
+    skipreprintrow:
+    cmp dh,25
+    jne reprintloop
+
+    popad
+    mov dl,0
+    ret
 
 
+Start:  mov bx, 000Fh   ;Page 0, colour attribute 15 (white) for the int 10 calls below
+        mov cx, 1       ;We will want to write 1 character
+        xor dx, dx      ;Start at top left corner
+        mov ds, dx      ;Ensure ds = 0 (to let us load the message)
+        cld             ;Ensure direction flag is cleared (for LODSB)
+ 
+                         ;PC BIOS Interrupt 10 Subfunction 2 - Set cursor position
+                         ;AH = 2
+ Char:  mov ah, 2       ;BH = page, DH = row, DL = column
+        int 10h
+        
+        mov dh,24
+        inc al
+        call printal
+        inc al
+        call printal
+        inc al
+        call printal
+        call printnl
+        ;lodsb           ;Load a byte of the message into AL.
+                        ;Remember that DS is 0 and SI holds the
+                        ;offset of one of the bytes of the message.
 
-Start:
-mov bx, 000Fh   ;Page 0, colour attribute 15 (white) for the int 10 calls below
-mov cx, 1       ;We will want to write 1 character
-xor dx, dx      ;Start at top left corner
-
-Char:
-inc dl          ;Advance cursor
-
-cmp dl, 32   ;80   ;Wrap around edge of screen if necessary
-jne Skip
-xor dl, dl
-inc dh
-
-cmp dh, 25      ;Wrap around bottom of screen if necessary
-jne Skip
-xor dh, dh
-
-mov ax,13h    ;320x200 screen mode
-int 10h       ;graphics interrupt
-
-Skip:
-;test for pixel rendering
-
-mov ax,0A000h ;video memory segment   
-mov es,ax     ;into es register
-
-mov ax,0 ;clear ax
-mov al,dh ;move y pos into al
-mov bl,160;move 160 (half screen width) into bl
-mul bl ;multiply y pos by bl (stored in ax)
-add ax,ax ;double result so it's actually times 320
-mov bx,0 ;clear bx
-mov bl,dl ;move x pos into bl
-add ax,bx ;add bx (x pos) to ax (y pos times width)
-mov di,ax ;set pixel location
-
-mov al,4      ;color 4 (red)
-mov [es:di],al        
-
-jmp Char
-
-times 0200h - 2 - ($ - $$)  db 0    ;Zerofill up to 510 bytes
-
-dw 0AA55h       ;Boot Sector signature
+                        ;PC BIOS Interrupt 10 Subfunction 9 - Write character and colour
+                        ;AH = 9
+        ;mov ah, 9       ;BH = page, AL = character, BL = attribute, CX = character count
+        ;int 10h
+ 
+ Skip:   jmp Char       ;otherwise restart from the beginning of the message
+ 
+ 
+ times 0200h - 2 - ($ - $$)  db 0    ;Zerofill up to 510 bytes
+ 
+         dw 0AA55h       ;Boot Sector signature
+ 
+ ;OPTIONAL:
+ ;To zerofill up to the size of a standard 1.44MB, 3.5" floppy disk
+ ;times 1474560 - ($ - $$) db 0
