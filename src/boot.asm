12a1fb4841ce845810432780ef4a417f5ad2b730
Nogardagem <understateddragon@gmail.com>
2025-09-23 17:37:05 -0400

    print stack's chars

--- src/boot.asm
+++ src/boot.asm
@@ -4,6 +4,18 @@ jmp short Start ;Jump over the data (the 'short' keyword makes the jmp instructi
 
 vmem equ 1000h
 
+printstack:
+    pop si ;remove function return address from stack
+    jmp endps
+startps:
+    call printal
+endps:
+    pop ax
+    cmp ax,0
+    jne startps
+push si ;put function return address back
+ret
+
 printal:
     pushad
     mov ah,2 ;cursor pos
@@ -12,42 +24,40 @@ printal:
     mov ah, 9 ;print
     int 10h
 
-    add dx,vmem
-    mov bl,0
-    push 0
+    add dx,vmem ;load letter memory pos
+    push 0 ;ensure es is 0
     pop es
     mov di,dx
-    mov [es:di],al
+    mov [es:di],al ;fill letter memory for letter
     
-    popad
+    popad ;ensures dx doesn't keep the memory pos we added
     inc dl
 ret
+
 printnl:
     pushad
 
-    push 0
+    push 0 ;ensure es is 0
     pop es
-    mov dx,0
-    mov dh,0
+    mov dx,0 ;start at top left
 reprintloop:
-    mov di,vmem
-    inc dh
+    mov di,vmem ;get address of letter memory loaded
+    
+    inc dh ;grab letter in next row
     add di,dx
     mov al,[es:di]
     dec dh
-    ;mov si,0
-    ;mov [es:di],si
     
-    call printal
-    
+    call printal ;print that letter in this row
+    ;printal moved to right already
     
     cmp dl,80
     jne skipreprintrow
-    inc dh
+    inc dh ;if at width, inc row
     mov dl,0
     skipreprintrow:
     cmp dh,25
-    jne reprintloop
+    jne reprintloop ;if not at height, continue
 
     popad
     mov dl,0
@@ -57,14 +67,19 @@ reprintloop:
 Start:  mov bx, 000Fh   ;Page 0, colour attribute 15 (white) for the int 10 calls below
         mov cx, 1       ;We will want to write 1 character
         xor dx, dx      ;Start at top left corner
-        mov ds, dx      ;Ensure ds = 0 (to let us load the message)
-        cld             ;Ensure direction flag is cleared (for LODSB)
  
                          ;PC BIOS Interrupt 10 Subfunction 2 - Set cursor position
                          ;AH = 2
  Char:  mov ah, 2       ;BH = page, DH = row, DL = column
         int 10h
         
+        push 0
+        push 'a'
+        push 'b'
+        push 'c'
+        
+        call printstack
+
         mov dh,24
         inc al
         call printal
