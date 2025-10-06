ad224c5b06a8101a606979e670f6f9c970063b41
Nogardagem <understateddragon@gmail.com>
2025-10-06 15:15:14 -0400

    will display text mode, but not fast?

--- src/os.bat
+++ src/os.bat
@@ -1,2 +1,4 @@
 "C:\Program Files\nasm\nasm" -f bin boot.asm -o boot.bin
-"C:\Program Files\qemu\qemu-system-i386" -drive file=boot.bin,index=0,format=raw -display gtk,zoom-to-fit=on
+"C:\Program Files\qemu\qemu-system-i386" -drive file=boot.bin,index=0,format=raw -display gtk,zoom-to-fit=on \
+-accel whpx,kernel-irqchip=off \
+-vga qxl
